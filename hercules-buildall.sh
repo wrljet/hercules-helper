939654098327915550adab58546b38a7f68bbf06
Bill Lewis <wrljet@gmail.com>
2023-06-28 10:55:02 -0400

    Improvement to default config files Make opt_install_dir respect an earlier setting of the same option.

--- hercules-buildall.sh
+++ hercules-buildall.sh
@@ -8,7 +8,7 @@
 #
 # https://github.com/wrljet/hercules-helper/blob/master/LICENSE
 
-# Updated: 27 JUN 2023
+# Updated: 28 JUN 2023
 VERSION_STR=v0.9.14+
 #
 # The most recent version of this project can be obtained with:
@@ -58,6 +58,9 @@ VERSION_STR=v0.9.14+
 #-----------------------------------------------------------------------------
 
 # Changelog:
+#
+# Updated: 28 JUN 2023
+# - corrections to default config files
 #
 # Updated: 27 JUN 2023
 # - run 'sudo ldconfig' after installation, if --sudo is in effect
